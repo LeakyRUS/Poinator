fe0a18e0f49f5da2960451efda464df4f908b35b
Владислав <vladislav.zhilin@mail.ru>
2019-05-06 10:52:19 +0300

    0.2.1

--- discord_poinator_bot/Modules/PublicModule.cs
+++ discord_poinator_bot/Modules/PublicModule.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Threading.Tasks;
+using System.Collections;
 using Discord;
 using Discord.Commands;
 using discord_poinator_bot.Services;
+using System.Collections.Generic;
+using static discord_poinator_bot.Static.ConstStrings.ApiSettings;
 
 namespace discord_poinator_bot.Modules
 {
@@ -11,14 +14,24 @@ namespace discord_poinator_bot.Modules
     {
         [Command("ping")]
         [Alias("!")]
+        //[RequireBotPermission(ChannelPermission.ManageMessages, ErrorMessage = "Gimme a manage messages permission!")]
         public async Task Hello(IUser user = null)
         {
             string answer = string.Empty;
+            var UserMessageId = Context.Message;
+
+            try
+            {
+                await UserMessageId.DeleteAsync();
+            }
+            catch (Exception e) { }
 
             user = user ?? Context.User;
-            answer += "Pong, <@" + user.Id.ToString() + ">";
+            answer += "*Pings* <@" + user.Id.ToString() + ">";
 
             await ReplyAsync(answer);
+
+            GetLog.ToConsole(Context, "Hello");
         }
 
         [Command("about")]
@@ -27,9 +40,10 @@ namespace discord_poinator_bot.Modules
         {
             string answer = string.Empty;
             answer += "I'm Poinator bot!\n";
-            answer += "Ver 0.1.1 alpha\n";
+            answer += "Ver 0.2.1 beta\n";
             answer += "Created by LeakyRUS#9272\n";
             answer += "Project repository: https://github.com/leakyrus/poinator \n";
+            answer += "Don't look at .gitignore\n";
             answer += "3rd party:\n";
             answer += "- zlib data compression library https://www.zlib.net/ \n";
             answer += "- Discord.Net https://github.com/discord-net/Discord.Net \n";
@@ -45,7 +59,9 @@ namespace discord_poinator_bot.Modules
 
             embed.WithColor(255, 255, 255);
 
-            await Context.Channel.SendMessageAsync("", embed: embed.Build());
+            await ReplyAsync(embed: embed.Build());
+
+            GetLog.ToConsole(Context, "About");
         }
 
         [Command("help")]
@@ -55,11 +71,12 @@ namespace discord_poinator_bot.Modules
             string answer = string.Empty;
 
             answer += "`help` (Alias: `?`) - the list of commands that the bot can do\n";
-            answer += "`ping` (Alias: `!`) - ping you\n";
+            answer += "`ping` **[user]** (Alias: `!`) - ping **you** or **[user]**\n";
             answer += "`about` (Alias: `/`) - brief\n";
-            answer += "`encode` **message** (Alias: `>`) - encoding your message to __Poi__ text. If __Poi__ text length bigger than 2k symbols then bot report an error\n";
-            answer += "`decode` **message** (Alias: `<`) - decoding your __Poi__ text to normal text\n";
-            answer += "`roll` **[message]** (Alias: `%`) - rolling between 1 and 20";
+            answer += "`encode` **<message>** (Alias: `>`) - encoding your **message** to __Poi__ text. Don't forget about 2k limit\n";
+            answer += "`decode` **<message>** (Alias: `<`) - decoding your __Poi__ text to normal text\n";
+            answer += "`roll` **[message]** (Alias: `%`) - rolling between 1 and 20 with **[message]**\n";
+            answer += "`msg` **[message]** (Alias: `$`) - send a message on behalf of the bot";
 
             var embed = new EmbedBuilder
             {
@@ -69,73 +86,145 @@ namespace discord_poinator_bot.Modules
 
             embed.WithColor(255, 255, 255);
 
-            await Context.Channel.SendMessageAsync("", embed: embed.Build());
+            await ReplyAsync(embed: embed.Build());
+
+            GetLog.ToConsole(Context, "Help");
         }
 
         [Command("encode")]
         [Alias(">")]
+        //[RequireBotPermission(ChannelPermission.ManageMessages, ErrorMessage = "Gimme a manage messages permission!")]
         //[RequireContext(ContextType.Guild)]
-        public async Task Encode([Remainder] string text)
+        public async Task Encode([Remainder] string text = null)
         {
-            string encoded = FromDll.Encode(text);
+            string encoded = text != null ? FromDll.Encode(text) : null;
             IUser user = Context.User;
+            var UserMessageId = Context.Message;
+
+            try
+            {
+                await UserMessageId.DeleteAsync();
+            }
+            catch (Exception e) { }
 
             var embed = new EmbedBuilder();
 
-            if (encoded.Length > 1950)
+            if (encoded != null)
             {
-                embed.WithTitle("Error!");
-                embed.WithDescription("Encoded text length is bigger than 2k symbols");
-                embed.WithColor(255, 128, 128);
+                if (encoded.Length > 1950)
+                {
+                    embed.WithTitle("Error!");
+                    embed.WithDescription("Encoded text length is bigger than 2k symbols");
+                    embed.WithColor(255, 128, 128);
+                }
+                else
+                {
+                    embed.WithTitle("Here is your encoded text!");
+                    embed.WithAuthor(user);
+                    embed.WithDescription($"```{encoded}```");
+                    embed.WithColor(128, 255, 128);
+                }
             }
             else
             {
-                embed.WithTitle("Here is your encoded text!");
-                embed.WithAuthor(user);
-                embed.WithDescription($"```{encoded}```");
-                embed.WithColor(128, 255, 128);
+                embed.WithTitle("Error!");
+                embed.WithDescription("Your text is empty");
+                embed.WithColor(255, 128, 128);
             }
 
-            if((text.Length + encoded.Length) < 1950) embed.WithFooter($"Your text: {text}");
-            else embed.WithFooter($"Your text is so big...");
+            if (encoded != null)
+            {
+                if ((text.Length + encoded.Length) < 1950) embed.WithFooter($"Your text: {text}");
+                else embed.WithFooter($"Your text is so big...");
+            }
 
-            await Context.Channel.SendMessageAsync("", embed: embed.Build());
+            await ReplyAsync(embed: embed.Build());
+
+            GetLog.ToConsole(Context, "Encode");
         }
 
         [Command("decode")]
         [Alias("<")]
-        public async Task Decode([Remainder] string text)
+        //[RequireBotPermission(ChannelPermission.ManageMessages, ErrorMessage = "Gimme a manage messages permission!")]
+        public async Task Decode([Remainder] string text = null)
         {
-            string decoded = FromDll.Decode(text);
+            string decoded = text != null ? FromDll.Decode(text) : null;
             IUser user = Context.User;
+            var UserMessageId = Context.Message;
+
+            try
+            {
+                await UserMessageId.DeleteAsync();
+            }
+            catch (Exception e) { }
 
             var embed = new EmbedBuilder();
 
-            embed.WithTitle("Here is your decoded text!");
-            embed.WithAuthor(user);
-            embed.WithDescription($"```{decoded}```");
-            embed.WithColor(128, 255, 128);
+            if (decoded != null)
+            {
+                embed.WithTitle("Here is your decoded text!");
+                embed.WithAuthor(user);
+                embed.WithDescription($"```{decoded}```");
+                embed.WithColor(128, 255, 128);
+            }
+            else
+            {
+                embed.WithTitle("Error!");
+                embed.WithDescription("Your text is empty");
+                embed.WithColor(255, 128, 128);
+            }
 
-            await Context.Channel.SendMessageAsync("", embed: embed.Build());
+            await ReplyAsync(embed: embed.Build());
+
+            GetLog.ToConsole(Context, "Decode");
         }
 
         [Command("roll")]
         [Alias("%")]
+        //[RequireBotPermission(ChannelPermission.ManageMessages, ErrorMessage = "Gimme a manage messages permission!")]
         public async Task Roll([Remainder] string text = null)
         {
             IUser user = Context.User;
             int roll = new Random((int)DateTime.Now.Ticks).Next(1, 21);
-            string rollMessage = String.Empty;
+            string rollMessage = string.Empty;
+            var UserMessageId = Context.Message;
+
+            try
+            {
+                await UserMessageId.DeleteAsync();
+            }
+            catch (Exception e) { }
 
             var embed = new EmbedBuilder();
-            rollMessage = "You roll " + roll.ToString();
+            rollMessage += "You roll " + roll.ToString();
             embed.WithAuthor(user);
             embed.WithColor((int)((roll <= 10) ? ((21 - roll) * 12.8 - 1) : 127), (int)((roll > 10) ? (roll * 12.8 - 1) : 127), 127);
 
             if (text != null) embed.WithTitle(rollMessage + " " + text);
             else embed.WithTitle(rollMessage);
 
-            await Context.Channel.SendMessageAsync("", embed: embed.Build());
+            await ReplyAsync(embed: embed.Build());
+
+            GetLog.ToConsole(Context, "Roll");
+        }
+
+        [Command("msg")]
+        [Alias("$")]
+        //[RequireBotPermission(ChannelPermission.ManageMessages, ErrorMessage = "Gimme a manage messages permission!")]
+        public async Task Message([Remainder] string text = null)
+        {
+            if(text != null)
+            {
+                await ReplyAsync(text);
+            }
+
+            try
+            {
+                await Context.Message.DeleteAsync();
+            }
+            catch (Exception e) { }
+
+            GetLog.ToConsole(Context, "Message");
         }
     }
 }
